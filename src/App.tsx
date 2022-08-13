9bc9c3ab7ee5f3f0e53b9534bf4663ca92b3de67
dataenggu <dsolkim1030@gmail.com>
2022-08-13 19:35:17 +0900

    init project structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./Header";
+import MainPage from "./Pages/MainPage";
+import NotFoundPage from "./Pages/NotFoundPage";
+import ContactPage from "./Pages/ContactPage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Pages/NotFoundPage.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+const NotFoundPage = () => {
+  return <div>페이지를 찾을 수 없슴</div>;
+};
+export default NotFoundPage;
