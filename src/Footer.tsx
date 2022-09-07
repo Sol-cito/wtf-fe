b5595a3ec81ed7429836dd68c258674c6e470c3f
dataenggu <dataenggu@gmail.com>
2022-09-07 15:11:21 +0900

    dummy

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -4,7 +4,7 @@ import { FOOTER_COPY_RIGHT } from "./models/TextConstant";
 const Footer = () => {
   return (
     <div className="footer">
-      <span>{FOOTER_COPY_RIGHT}</span>
+      <span> {FOOTER_COPY_RIGHT} </span>
     </div>
   );
 };
