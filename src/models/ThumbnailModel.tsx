2d0b0900a400b582c8f5628824ac01d62c8300c7
dataenggu <dataenggu@gmail.com>
2022-09-07 17:09:12 +0900

    jenkins test

--- src/models/ThumbnailModel.tsx
+++ src/models/ThumbnailModel.tsx
@@ -2,7 +2,7 @@ const THUMBNAIL_DATA = [
   {
     imgUrl: "img/thumbnail_1.jpg",
     linkUrl: "/projectPage",
-    text: "Explanation of design",
+    text: "Explanation of the design should be here",
   },
   {
     imgUrl: "img/thumbnail_2.jpg",
