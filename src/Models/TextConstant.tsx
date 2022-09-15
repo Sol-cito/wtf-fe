78080b7b723f57b9b5c5f5295abc170d0eaba2dd
dataenggu <dataenggu@gmail.com>
2022-09-15 16:23:58 +0900

    dummy

---
+++ src/Models/TextConstant.tsx
@@ -0,0 +1,7 @@
+export const MAIN_PAGE_BOX_TYPING: string = "Woori Team Fighting...!!";
+
+export const PAGE_NOT_FOUND: string = "Page Not Found";
+
+export const GO_BACK_HOME: string = "홈으로";
+
+export const FOOTER_COPY_RIGHT: string = "Copyright All Rights Reserved 2022";
