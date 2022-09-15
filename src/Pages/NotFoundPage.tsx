2e193eb74af12108766ebb80b7d918586749e1a3
dataenggu <dataenggu@gmail.com>
2022-09-15 13:53:42 +0900

    Complete not found page

--- src/Pages/NotFoundPage.tsx
+++ src/Pages/NotFoundPage.tsx
@@ -1,6 +1,14 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import "./NotFoundPage.scss";
 
 const NotFoundPage = () => {
-  return <div>Not Found</div>;
+  return (
+    <div className="not_found_container">
+      <h1>Page Not Found</h1>
+      <div>
+        <Link to={"/"}>홈으로</Link>
+      </div>
+    </div>
+  );
 };
 export default NotFoundPage;
