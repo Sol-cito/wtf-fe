b761c5b8f75cb28baf181b15703e0b04191e5727
dataenggu <dsolkim1030@gmail.com>
2022-09-27 15:20:41 +0900

    add css to player modal

--- src/Components/PlayerModalBox.tsx
+++ src/Components/PlayerModalBox.tsx
@@ -1,3 +1,4 @@
+import moment from "moment";
 import { useAppSelector } from "../Store/config";
 import "./PlayerModalBox.scss";
 
@@ -10,14 +11,23 @@ const PlayerModalBox = () => {
         <div className="player_info_container">
           <div className="photo_area">
             {/* <img src={player.profileImgSrc} /> */}
-            <img src="img/player/inhwan_profile_no_background.png" />
+            <img src="img/player/wonnam_profile_no_background.png" />
           </div>
           <div className="info_area">
-            <div>{player.name}</div>
-            <div>{player.backNo}</div>
-            <div>{player.position}</div>
-            <div>{player.birth}</div>
-            <div>{player.moto}</div>
+            <div className="moto_area">"{player.moto}"</div>
+            <div className="basic_info_area">
+              <span id="back_no">{player.backNo}</span>
+              <span id="position">{player.position}</span>
+              <span id="name">{player.name}</span>
+            </div>
+            <div className="eng_name_area">
+              <span id="first_name_eng">{player.firstNameEng}</span>
+              <span id="family_name_eng">{player.familyNameEng}</span>
+            </div>
+            <div className="birth_area">
+              <span id="birth">생년월일 | </span>{" "}
+              {moment(player.birth).format("YYYY-MM-DD")}
+            </div>
           </div>
         </div>
       ) : null}
