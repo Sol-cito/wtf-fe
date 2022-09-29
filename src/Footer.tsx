f9a267df9d5955e722b186181b31ac71db08ea03
dataenggu <dataenggu@gmail.com>
2022-09-29 09:59:57 +0900

    Add sns icons and links

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -5,6 +5,14 @@ const Footer = () => {
   return (
     <div className="footer">
       <span>{FOOTER_COPY_RIGHT}</span>
+      <div className="icon_area">
+        <a href="https://www.instagram.com/yonsei_wtf/" target="_blank">
+          <img src="img/etc/instagram_icon.png" />
+        </a>
+        <a href="https://www.facebook.com/yonseisoccerwtf/" target="_blank">
+          <img src="img/etc/facebook_icon.png" />
+        </a>
+      </div>
     </div>
   );
 };
