07379d6ef6912841bd88f9ac15bcd1c9bfb2a2d8
dataenggu <dataenggu@gmail.com>
2022-10-13 09:47:09 +0900

    adjust css

--- src/CommonConstant/ImgSrcConstant.tsx
+++ src/CommonConstant/ImgSrcConstant.tsx
@@ -2,7 +2,8 @@ export const MAIN_TEAM_PHOTO_SRC = "img/team/team_photo_2.jpg";
 
 export const PLAYER_MODAL_BACKGROUND_STYLE_BROWSER = {
   background: "url(/img/etc/player_modal_background.jpg)",
-  backgroundSize: "cover",
+  backgroundSize: "contain",
+  backgroundRepeat: "no-repeat",
 };
 
 export const PLAYER_MODAL_BACKGROUND_STYLE_MOBILE = {
