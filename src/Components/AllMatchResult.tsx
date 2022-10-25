87ccabcc72c142ecd02517555c9908c9eb9a70ef
dataenggu <dataenggu@gmail.com>
2022-10-25 10:18:58 +0900

    add infinite scroll on match result page

--- src/Components/AllMatchResult.tsx
+++ src/Components/AllMatchResult.tsx
@@ -5,18 +5,27 @@ import { MatchResultModel } from "../Models/MatchResultModel";
 import { getMatchResultAPI, MatchResultProps } from "../Service/MatchService";
 import "./AllMatchResult.scss";
 import CustomizedSpinner from "./CustomizedSpinner";
+import { useInView } from "react-intersection-observer";
 
 const AllMatchResult = () => {
-  const MATCH_RESULTLIMIT: number = 3;
+  const MATCH_RESULTLIMIT: number = 5;
+  const ORDER_ENTITY_FIELD_NAME = "matchDate";
+
+  const [inViewRef, inView] = useInView();
 
   const [matchResult, setMatchResult] = useState<MatchResultModel[]>([]);
   const [isLoading, setIsLoding] = useState<boolean>(true);
 
+  const [numberOfLastResultData, setNumberOfLastResultData] =
+    useState<number>();
+
   const [resultStartIdx, setResultStartIdx] = useState<number>(0);
 
-  const handleShowMoreOnClick = () => {
-    setResultStartIdx(resultStartIdx + 3);
-  };
+  useEffect(() => {
+    if (inView && !isLoading && numberOfLastResultData === MATCH_RESULTLIMIT) {
+      setResultStartIdx(resultStartIdx + MATCH_RESULTLIMIT);
+    }
+  }, [inView]);
 
   useEffect(() => {
     getMatchResult();
@@ -28,33 +37,25 @@ const AllMatchResult = () => {
       startIdx: resultStartIdx,
       limit: MATCH_RESULTLIMIT,
       order: {
-        entityFieldName: "matchDate",
+        entityFieldName: ORDER_ENTITY_FIELD_NAME,
         orderSortKeyword: OrderSortKeyword.DESC,
       },
     };
-    const res = await getMatchResultAPI(matchResultProps);
+    const res: MatchResultModel[] = await getMatchResultAPI(matchResultProps);
     if (res) {
-      setMatchResult(matchResult.concat(res));
+      setMatchResult([...matchResult, ...res]);
+      setNumberOfLastResultData(res.length);
       setIsLoding(false);
     }
   };
 
-  useEffect(() => {
-    getMatchResult();
-  }, []);
-
   return (
     <>
-      {isLoading ? (
-        <CustomizedSpinner />
-      ) : (
-        matchResult.map((ele, idx) => {
-          return <MatchResult matchResult={ele} key={idx} />;
-        })
-      )}
-      <div className="show_more" onClick={handleShowMoreOnClick}>
-        더 보기
-      </div>
+      {matchResult.map((ele, idx) => {
+        return <MatchResult matchResult={ele} key={idx} />;
+      })}
+      {isLoading ? <CustomizedSpinner /> : null}
+      <div className="show_more" ref={inViewRef} />
     </>
   );
 };
