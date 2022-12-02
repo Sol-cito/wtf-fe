2bf1ba9e65e75e84f3b8808f7ae2ee5344f983d1
dataenggu <dataenggu@gmail.com>
2022-12-02 15:41:30 +0900

    create team history and board member component

---
+++ src/Components/BoardMemberBox.tsx
@@ -0,0 +1,25 @@
+import { BoardMemberModel } from "../Models/BoardMemberModel";
+import "./BoardMemberBox.scss";
+
+export interface BoardMemberBoxProps {
+  boardMemberList: BoardMemberModel[];
+}
+
+const BoardMemberBox = (props: BoardMemberBoxProps) => {
+  return (
+    <div className="board_member_container">
+      {props.boardMemberList.map.length > 0
+        ? props.boardMemberList.map((ele, idx) => {
+            return (
+              <div key={idx} className="member">
+                <span id="board_name"> {ele.boardName}</span>
+                <span id="player_name"> {ele.player.name}</span>
+                <span id="assigned_date"> ({ele.assignedDate}~) </span>
+              </div>
+            );
+          })
+        : null}
+    </div>
+  );
+};
+export default BoardMemberBox;
